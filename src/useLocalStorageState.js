53cd91f1d98d3dea815cf8bf02cd9d67019fe4fb
Shivaraj Avate <shivaraj.avate@mantle3d.com>
2024-07-05 23:40:12 +0530

    feat: create useLocalStorageState for localStorage

---
+++ src/useLocalStorageState.js
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+export function useLocalStorageState(key, intialState = tempWatchedData) {
+  const [value, setValue] = useState(() => {
+    const storedValue = window.localStorage.getItem(key);
+    if (storedValue && JSON.parse(storedValue).length) {
+      return JSON.parse(storedValue);
+    } else {
+      return intialState;
+    }
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
